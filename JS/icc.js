def07989490913a08c920d706786349c427de516
Azula Dev <caligeekweb@gmail.com>
2023-09-11 10:32:36 -0500

    mostrando resultados IMC en pantalla

--- JS/icc.js
+++ JS/icc.js
@@ -10,7 +10,6 @@ function limitarNumero(input, maxLength) {
   };
 
 //   Tomando el dato ingresado en el input
-
 const calculoIcc = () =>{
     
     let cintura = document.getElementById("cintura").value;
@@ -19,5 +18,37 @@ const calculoIcc = () =>{
     
     const formulaIcc = (cintura/cadera).toFixed(2);
     document.getElementById("resultadoIcc").innerHTML = formulaIcc;
-    
+
+
+    switch (true) {
+
+      case formulaIcc < 18.5:
+        resultado  = "Esto se clasifica como: BAJO PESO";
+        break;
+        
+      case formulaIcc  >= 18.5 && formulaIcc <= 24.9:
+        resultado  = "Esto se clasifica como: PESO NORMAL";
+        break;
+        
+      case formulaIcc  >= 25 && formulaIcc <= 29.9:
+        resultado  = "Esto se clasifica como: OBESIDAD";
+        break;
+
+      case formulaIcc  >= 30 && formulaIcc <= 34.9:
+        resultado  = "Esto se clasifica como: OBESIDAD GRADO I";
+        break;
+        
+      case formulaIcc  >= 35 && formulaIcc <= 39.9:
+        resultado  = "Esto se clasifica como: OBESIDAD GRADO II";
+        break;
+  
+      case formulaIcc  >= 40:
+        resultado  = "Esto se clasifica como: OBESIDAD GRADO III";
+        break;
+
+      default:
+        resultado = " verifica tus datos"
+
+}
+  document.getElementById("resultadoImc").innerHTML += ` ${resultado}`;
 }
--- JS/imc.js
+++ JS/imc.js
@@ -12,19 +12,48 @@ function limitarNumero(input, maxLength) {
 //   Tomando el dato ingresado en el input
 
 const calculoImc = () =>{
+  console.log("La función calculoImc se está ejecutando");
     
     let talla = document.getElementById("talla").value;
     let tallaDecimal = parseFloat(talla);
     let peso = document.getElementById("peso").value;
-    
+    let resultado = '';
     
     const formulaImc = (Math.floor (peso / (tallaDecimal*tallaDecimal)) );
     document.getElementById("resultadoImc").innerHTML = formulaImc;
-
-    if(formulaImc >30 && formulaImc < 34.9){
-      document.getElementById("riesgoImc").innerHTML = "riesgo";
-    }else{
-      document.getElementById("riesgoImc").innerHTML = "sin riesgo";
-    }
+   
     
+
+    switch (true) {
+
+      case formulaImc < 18.5:
+        resultado  = "Esto se clasifica como: BAJO PESO";
+        break;
+        
+      case formulaImc  >= 18.5 && formulaImc <= 24.9:
+        resultado  = "Esto se clasifica como: PESO NORMAL";
+        break;
+        
+      case formulaImc  >= 25 && formulaImc <= 29.9:
+        resultado  = "Esto se clasifica como: OBESIDAD";
+        break;
+
+      case formulaImc  >= 30 && formulaImc <= 34.9:
+        resultado  = "Esto se clasifica como: OBESIDAD GRADO I";
+        break;
+        
+      case formulaImc  >= 35 && formulaImc <= 39.9:
+        resultado  = "Esto se clasifica como: OBESIDAD GRADO II";
+        break;
+  
+      case formulaImc  >= 40:
+        resultado  = "Esto se clasifica como: OBESIDAD GRADO III";
+        break;
+
+      default:
+        resultado = " verifica tus datos"
+        
+}
+
+    document.getElementById("resultadoImc").innerHTML += ` ${resultado}`;
 }
